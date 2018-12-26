0671bac4ce7a33960b98c4b4fc53eeb0a47866a7
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2018-12-26 22:01:39 +0200

    restored action to perform language switch

--- tests/DbLocalizationProvider.Core.AspNetSample/Controllers/HomeController.cs
+++ tests/DbLocalizationProvider.Core.AspNetSample/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using DbLocalizationProvider.Core.AspNetSample.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
@@ -37,6 +40,20 @@ namespace DbLocalizationProvider.Core.AspNetSample.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult SetLanguage(string culture, string returnUrl)
+        {
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                                    CookieRequestCultureProvider.MakeCookieValue(new RequestCulture(culture)),
+                                    new CookieOptions
+                                    {
+                                        Expires = DateTimeOffset.UtcNow.AddYears(1)
+                                    }
+                                   );
+
+            return LocalRedirect(returnUrl);
         }
     }
 }
