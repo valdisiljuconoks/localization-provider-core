8bd73fbdf1481ccf66e6c34170189314af33f2f2
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2020-01-08 11:05:25 +0200

    Changed version Changed license

--- src/DbLocalizationProvider.AspNetCore/DbStringLocalizerFactory.cs
+++ src/DbLocalizationProvider.AspNetCore/DbStringLocalizerFactory.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Valdis Iljuconoks. All rights reserved.
+// Licensed under Apache-2.0. See the LICENSE file in the project root for more information
+
+using System;
 using Microsoft.Extensions.Localization;
 
 namespace DbLocalizationProvider.AspNetCore
