1300b98dd344657d9fd194dfc5125d283bce60c9
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2021-02-14 22:37:35 +0200

    Added Csv formatter project to the pack process

--- build-packages.ps1
+++ build-packages.ps1
@@ -12,4 +12,10 @@ copy .\bin\Release\LocalizationProvider.AdminUI.AspNetCore.*.nupkg .\..\..\.nuge
 copy .\bin\Release\LocalizationProvider.AdminUI.AspNetCore.*.snupkg .\..\..\.nuget\
 cd .\..\..\.nuget\
 
+cd .\..\src\DbLocalizationProvider.AdminUI.AspNetCore.Csv\
+dotnet pack -c Release
+copy .\bin\Release\LocalizationProvider.AdminUI.AspNetCore.Csv.*.nupkg .\..\..\.nuget\
+copy .\bin\Release\LocalizationProvider.AdminUI.AspNetCore.Csv.*.snupkg .\..\..\.nuget\
+cd .\..\..\.nuget\
+
 cd ..\
