6fd9be1e87d0e8df6b6d6709b00aa52f818b79b9
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2022-02-09 09:14:38 +0200

    set version to 7.1

--- push-packages.ps1
+++ push-packages.ps1
@@ -1,8 +1,8 @@
 cd .\.nuget
 
-.\nuget.exe push LocalizationProvider.AspNetCore.7.0.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.Csv.7.0.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.Xliff.7.0.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.7.0.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.AspNetCore.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.Csv.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.Xliff.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
+.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.7.1.0.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
