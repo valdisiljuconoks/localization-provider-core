8d5420534f396e1ccd9e3984d0649ed2c23837ca
valdisiljuconoks <valdis.iljuconoks@outlook.com>
2022-03-15 16:42:45 +0200

    Fixed possible ILogger absence

--- src/DbLocalizationProvider.AspNetCore/LoggerAdapter.cs
+++ src/DbLocalizationProvider.AspNetCore/LoggerAdapter.cs
@@ -18,22 +18,22 @@ namespace DbLocalizationProvider.AspNetCore
 
         public void Debug(string message)
         {
-            _logger.LogDebug(message);
+            _logger?.LogDebug(message);
         }
 
         public void Info(string message)
         {
-            _logger.LogInformation(message);
+            _logger?.LogInformation(message);
         }
 
         public void Error(string message)
         {
-            _logger.LogError(message);
+            _logger?.LogError(message);
         }
 
         public void Error(string message, Exception exception)
         {
-            _logger.LogError(message, exception);
+            _logger?.LogError(message, exception);
         }
     }
 }
