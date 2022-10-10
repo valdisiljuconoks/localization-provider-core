62d2bc3f92c9b6042a8a7bbc9b7efb3f62e0743a
ruwen.jin <ruwen.jin@aurumio.com>
2022-10-10 19:30:59 +0200

    Enable import

--- src/DbLocalizationProvider.AdminUI.AspNetCore/ServiceController.cs
+++ src/DbLocalizationProvider.AdminUI.AspNetCore/ServiceController.cs
@@ -4,16 +4,21 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using DbLocalizationProvider.Abstractions;
 using DbLocalizationProvider.AdminUI.AspNetCore.Models;
 using DbLocalizationProvider.AdminUI.AspNetCore.Routing;
 using DbLocalizationProvider.AdminUI.AspNetCore.Security;
 using DbLocalizationProvider.AdminUI.Models;
+using DbLocalizationProvider.AspNetCore.Import;
 using DbLocalizationProvider.Commands;
+using DbLocalizationProvider.Import;
 using DbLocalizationProvider.Queries;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace DbLocalizationProvider.AdminUI.AspNetCore
 {
@@ -24,12 +29,19 @@ namespace DbLocalizationProvider.AdminUI.AspNetCore
         private readonly UiConfigurationContext _config;
         private readonly ICommandExecutor _commandExecutor;
         private readonly IQueryExecutor _queryExecutor;
+        private readonly ConfigurationContext _configurationContext;
 
-        public ServiceController(UiConfigurationContext config, ICommandExecutor commandExecutor, IQueryExecutor queryExecutor)
+        public ServiceController(
+            UiConfigurationContext config, 
+            ICommandExecutor commandExecutor, 
+            IQueryExecutor queryExecutor,
+            ConfigurationContext configurationContext
+            )
         {
             _config = config;
             _commandExecutor = commandExecutor ?? throw new ArgumentNullException(nameof(commandExecutor));
             _queryExecutor = queryExecutor ?? throw new ArgumentNullException(nameof(queryExecutor));
+            _configurationContext = configurationContext;
         }
 
         [HttpGet]
@@ -67,6 +79,44 @@ namespace DbLocalizationProvider.AdminUI.AspNetCore
             _commandExecutor.Execute(createNewResourceCmd);
 
             return ServiceOperationResult.Ok;
+        }
+
+        /// <inheritdoc/>
+        [HttpPost]
+        [Route("validate", Name = "LocAdminValidate")]
+        public ValidationResponse ValidateFile(IFormFile importFile)
+        {
+            var streamReader = new StreamReader(importFile.OpenReadStream());
+            var fileContent = streamReader.ReadToEnd();
+           
+            
+            if (string.IsNullOrEmpty(fileContent))
+            {
+                return new ValidationResponse(null);
+            }
+            var parser = _configurationContext.Import.Providers.FindByExtension(".json");
+
+            var result = parser.Parse(fileContent);
+
+            var workflow = new ResourceImportWorkflow(_commandExecutor, _queryExecutor);
+
+            var (resources, languages) = GetResources();
+            var detectedImportChanges = workflow.DetectChanges(result.Resources, resources);
+
+            return new ValidationResponse(detectedImportChanges.Where(x=>(x.ImportingResource.IsHidden!=null && !x.ImportingResource.IsHidden.Value)).ToArray());
+        }
+
+        /// <inheritdoc/>
+        [HttpPost]
+        [Route("import", Name = "LocAdminImport")]
+        public JsonResult ImportFile([ModelBinder(typeof(NewtonJsonModelBinder))][FromBody]List<DetectedImportChange> changes)
+        {
+            var detectedImportChanges = changes.Where(c => c.Selected).ToList();
+
+            var workflow = new ResourceImportWorkflow(_commandExecutor, _queryExecutor);
+            var messages = workflow.ImportChanges(detectedImportChanges).ToList();
+           
+            return new JsonResult(messages);
         }
 
         [HttpPost]
