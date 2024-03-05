2f91ab0661b3e1e3d07664b31f9ed7c741a81a88
Valdis Iljuconoks <valdis.iljuconoks@outlook.com>
2024-03-05 08:17:31 +0200

    set 8.0.1 in publish script

--- push-packages.ps1
+++ push-packages.ps1
@@ -1,8 +1,11 @@
+# nuget setapikey .... -Source https://www.nuget.org/api/v2/package
+# nuget setapikey .... -Source https://www.nuget.org/api/v2/symbolpackage
+
 cd .\.nuget
 
-.\nuget.exe push LocalizationProvider.AspNetCore.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.Csv.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
-.\nuget.exe push LocalizationProvider.AdminUI.AspNetCore.8.0.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AspNetCore.8.0.1.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.0.1.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.0.1.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.8.0.1.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
