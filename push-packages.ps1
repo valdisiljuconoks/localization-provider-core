4446ac3d5fabedaca0d0cfed81cabac0e2e72a0e
Valdis Iljuconoks <valdis@getadigital.com>
2024-06-05 21:34:04 +0300

    set version to 8.1 upgrade nugets

--- push-packages.ps1
+++ push-packages.ps1
@@ -3,9 +3,9 @@
 
 cd .\.nuget
 
-nuget push LocalizationProvider.AspNetCore.8.0.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.0.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.0.3.nupkg -source https://api.nuget.org/v3/index.json
-nuget push LocalizationProvider.AdminUI.AspNetCore.8.0.3.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AspNetCore.8.1.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Csv.8.1.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.Xliff.8.1.0.nupkg -source https://api.nuget.org/v3/index.json
+nuget push LocalizationProvider.AdminUI.AspNetCore.8.1.0.nupkg -source https://api.nuget.org/v3/index.json
 
 cd ..\
